8dba85ed6fc0ac33ac01eda22736a4e8afa93e8c
pmarxbraun <p.marinbraun@gmail.com>
2020-08-01 17:51:17 +0200

    Delete console.log on client JS file

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side JS file");
-
 const weatherFrom = document.querySelector("form");
 const searchForm = document.querySelector("input");
 const messageOne = document.querySelector("#messageOne");
